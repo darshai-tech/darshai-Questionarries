acc06ca4b45de727a438147d5ba76e04be18793d
Rkdash07 <rupeshkumardash1@gmail.com>
2026-05-03 22:37:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#0F766E", // premium teal
+        accent: "#F59E0B",
+        dark: "#0B132B",
+      },
+    },
+  },
+  plugins: [],
+}
